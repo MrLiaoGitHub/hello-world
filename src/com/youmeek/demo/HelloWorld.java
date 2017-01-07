5ba2a09598347f307ac97e3e0459c2f34b4e2e24
MrLiaoGitHub <hard_tornado@163.com>
2017-01-07 14:52:23 +0800

    commit test!

--- src/com/youmeek/demo/HelloWorld.java
+++ src/com/youmeek/demo/HelloWorld.java
@@ -5,6 +5,9 @@ package com.youmeek.demo;
  */
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        print();
+    }
+    public static void print(){
+        System.out.println("Just test");
     }
 }
