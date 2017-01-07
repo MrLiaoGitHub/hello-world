e5f34e9d73de9812329de94383778fbdb306e7b6
Liao Ziqiang(廖子强) <liaoziqiang@gj.com>
2017-01-07 14:30:03 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * Created by liaoziqiang on 2017-01-07.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
